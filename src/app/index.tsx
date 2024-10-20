93d27aa14c15c77afd9fee5de1c7e54b59455b75
Victor Hugo Gonçalves de Oliveira <65239846+victoroliveira13@users.noreply.github.com>
2024-10-20 12:47:44 -0300

    Add files via upload

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-xl">@victor.oliveira13</Text>
+    </View>
+  );
+}
